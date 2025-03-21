0d8bc9e01314a844254905c147e396c499de9851
elliotjharper <50680194+elliotjharper@users.noreply.github.com>
2025-03-21 20:24:21 +0000

    Wrote the matrix into the keyboard.json and the keymap.c file

--- split-tkl/qmk/keyboards/split_tkl/keymaps/elliotjharper/keymap.c
+++ split-tkl/qmk/keyboards/split_tkl/keymaps/elliotjharper/keymap.c
@@ -3,26 +3,14 @@
 
 #include QMK_KEYBOARD_H
 
-
+//      C1         C2        C3        C4        C5        C6        C7        C8        C9        C10       C11       C12       C13        C14        C15        C16         C17         C18
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
-     * │Esc│   │F1 │F2 │F3 │F4 │ │F5 │F6 │F7 │F8 │ │F9 │F10│F11│F12│ │PSc│Scr│Pse│
-     * └───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐
-     * │ ` │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │ Backsp│ │Ins│Hom│PgU│
-     * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤
-     * │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │     │ │Del│End│PgD│
-     * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┐ Ent│ └───┴───┴───┘
-     * │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │ # │    │
-     * ├────┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┴────┤     ┌───┐
-     * │Shft│ \ │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │    Shift │     │ ↑ │
-     * ├────┼───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐
-     * │Ctrl│GUI │Alt │                        │ Alt│ GUI│Menu│Ctrl│ │ ← │ ↓ │ → │
-     * └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘
-     */
     [0] = LAYOUT(
-        KC_1,    KC_2,    KC_3,
-        KC_4,    KC_5,    KC_6
+        KC_ESC,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_NO,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_F11,    KC_F12,    KC_PSCR,    KC_SCRL,    KC_PAUSE,
+        KC_GRAVE,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINUS,  KC_EQUAL,  KC_BSPC,   KC_INS,     KC_HOME,    KC_PGUP,
+        KC_TAB,    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC,   KC_ENT,    KC_DEL,     KC_END,     KC_PGDN,
+        KC_NO,     KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,   KC_QUOT,   KC_NUHS,   KC_NO,      KC_NO,      KC_NO,
+        KC_LSFT,   KC_BSLS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,   KC_NO,     KC_RSFT,   KC_NO,      KC_UP,      KC_NO,
+        KC_LCTL,   KC_LGUI,  KC_NO,    KC_LALT,  KC_NO,    KC_SPC,   KC_NO,    KC_NO,    KC_SPC,   KC_NO,    KC_NO,    KC_RALT,  KC_NO,     KC_NO,     KC_RCTL,   KC_LEFT,    KC_DOWN,    KC_RIGHT
     )
 };
